208becc346eb70c71d4cc18fbe01781a888d270d
vleadadmin <admin@vlabs.ac.in>
2023-12-15 05:41:30 +0000

    https://virtual-labs.github.io/virtual-labs/exp-kelvin-sdouble-bridge-iitr click on the link to test your code.

---
+++ postest.md
@@ -0,0 +1,40 @@
+## Post Test
+#### Please attempt the following questions
+
+Q1. The relation between ratio of resistance arms and ratio of resistance arms of second bridge is _________.<br>
+
+a.   Unequal<br>
+b.   Equal<br>
+c.   Twice<br>
+d.   One forth<br>
+
+Q2. Why Kelvin bridge is used for measurement of low resistance?<br> 
+
+a.   Due to e.m.f source used<br>
+b.   Due to a large current flow<br>
+c.   Due to contact and lead resistance<br>
+d.   Due to power dissipation across the circuit<br>
+
+Q3. What is the condition to achieve a high sensitivity in a Kelvin bridge?<br>
+
+a.   Low voltage<br>
+b.   High power<br>
+c.   Medium resistance<br>
+d.   High current<br>
+
+Q4.  Kelvin bridge can be calibrated to read _________.<br>
+
+a.   Inductance and Quality factor value<br>
+b.   Capacitance only<br>
+c.   Power and Voltage<br>
+d.   Current and frequency<br>
+
+Q5. Why Kelvin bridge cannot be used for the measurement of low Quality factor value?<br>
+
+a.   Due to thermoelectric effect<br>
+b.   Due to balance problem<br>
+c.   Due to the dull detector used<br>
+d.   Due to temperature<br>
+
+
+
---
+++ simulation/js/demo2.js
@@ -0,0 +1,147 @@
+//Scripting of needle range begins
+var rangeClock1 =  document.querySelector('#needle1');
+   
+function rangeChange138() {
+       rangeClock1.style.transform = 'rotate(-44deg)';
+      
+   }
+   function rangeChange139() {
+     rangeClock1.style.transform = 'rotate(-47deg)';     
+ }
+ function rangeChange1310() {
+   rangeClock1.style.transform = 'rotate(-50deg)';
+ }
+  function rangeChange1311() {
+   rangeClock1.style.transform = 'rotate(-52deg)';
+ }
+ function rangeChange1312() {
+   rangeClock1.style.transform = 'rotate(-55deg)';
+ }
+  function rangeChange148() {
+   rangeClock1.style.transform = 'rotate(-49deg)';
+ }
+  function rangeChange149() {
+   rangeClock1.style.transform = 'rotate(-51deg)';
+ }
+  function rangeChange1410() {
+   rangeClock1.style.transform = 'rotate(-54deg)';
+ }
+  function rangeChange1411() {
+   rangeClock1.style.transform = 'rotate(-56deg)';
+ }
+ function rangeChange1412() {
+   rangeClock1.style.transform = 'rotate(-58deg)';
+ }
+ function rangeChange269() {
+  rangeClock1.style.transform = 'rotate(-46deg)';
+}
+function rangeChange2611() {
+  rangeClock1.style.transform = 'rotate(-48deg)';
+}
+function rangeChange2612() {
+  rangeClock1.style.transform = 'rotate(-49deg)';
+}
+ function rangeChange158() {
+    rangeClock1.style.transform = 'rotate(-45deg)';
+  }
+   function rangeChange159() {
+    rangeClock1.style.transform = 'rotate(-41deg)';
+  }
+   function rangeChange1510() {
+    rangeClock1.style.transform = 'rotate(-37deg)';
+  }
+   function rangeChange1511() {
+    rangeClock1.style.transform = 'rotate(-33deg)';
+  }
+  function rangeChange1512() {
+    rangeClock1.style.transform = 'rotate(-29deg)';
+  }
+  function rangeChange168() {
+    rangeClock1.style.transform = 'rotate(-26deg)';
+  }
+   function rangeChange169() {
+    rangeClock1.style.transform = 'rotate(-23deg)';
+  }
+   function rangeChange1610() {
+    rangeClock1.style.transform = 'rotate(-21deg)';
+  }
+   function rangeChange1611() {
+    rangeClock1.style.transform = 'rotate(-18deg)';
+  }
+  function rangeChange1612() {
+    rangeClock1.style.transform = 'rotate(-15deg)';
+  }
+  function rangeChange178() {
+    rangeClock1.style.transform = 'rotate(-14deg)';
+  }
+   function rangeChange179() {
+    rangeClock1.style.transform = 'rotate(-11deg)';
+  }
+   function rangeChange1710() {
+    rangeClock1.style.transform = 'rotate(-8deg)';
+  }
+   function rangeChange1711() {
+    rangeClock1.style.transform = 'rotate(-5deg)';
+  }
+  function rangeChange1712() {
+    rangeClock1.style.transform = 'rotate(0deg)';
+  }
+ 
+ 
+//Scripting of needle range ends
+
+var range90 =  document.querySelector('#knob1');
+var range91 =  document.querySelector('#knob2');
+var range92 =  document.querySelector('#knob3');
+
+ function range901() {
+        range90.style.transform = 'rotate(180deg)';
+    
+    }
+    function range902() {
+      range90.style.transform = 'rotate(245deg)'; 
+      
+  }
+    
+     function range903() {
+        
+        range91.style.transform = 'rotate(87deg)';
+        
+    } function range904() {
+
+        range91.style.transform = 'rotate(116deg)';
+    
+    } function range905() {
+        
+        range91.style.transform = 'rotate(180deg)';
+        
+    }
+     function range906() {
+        range91.style.transform = 'rotate(242deg)';
+        
+    }
+     function range907() {
+        
+        range91.style.transform = 'rotate(300deg)';
+        
+    }
+     function range908() {
+    
+        range92.style.transform = 'rotate(87deg)';
+    }
+     function range909() {
+        
+        range92.style.transform = 'rotate(116deg)';
+    }
+     function range910() {
+    
+        range92.style.transform = 'rotate(148deg)';
+    }
+     function range911() {
+        
+        range92.style.transform = 'rotate(180deg)';
+    }
+     function range912() {
+    
+        range92.style.transform = 'rotate(210deg)';
+    }
